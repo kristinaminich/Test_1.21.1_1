704f15844f493329d5f07daebf0779ebcbb874a7
felix.hartig <kristina@minich.eu>
2024-09-14 15:13:59 +0200

    s

---
+++ src/client/java/Felix/test_1_21_1/client/Test_1_21_1Client.java
@@ -0,0 +1,10 @@
+package Felix.test_1_21_1.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Test_1_21_1Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/Felix/test_1_21_1/Test_1_21_1.java
@@ -0,0 +1,10 @@
+package Felix.test_1_21_1;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Test_1_21_1 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}

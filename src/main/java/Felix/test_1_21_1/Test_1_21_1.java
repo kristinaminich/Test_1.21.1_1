b6b2c275e8ebad6d919b50b02a9d6bbc27e13412
admin <kristina@minich.eu>
2024-10-05 16:52:23 +0200

    s

--- src/main/java/Felix/test_1_21_1/Test_1_21_1.java
+++ src/main/java/Felix/test_1_21_1/Test_1_21_1.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test_1_21_1 implements ModInitializer {
-    public static final String MODID = "test_1.21.1";
+    public static final String MODID = "test";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     public static RegistryHelper registryHelper = new RegistryHelper();
 

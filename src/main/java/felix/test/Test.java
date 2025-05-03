42a9762f99a17ce076270dd685322a8f7ce536bb
Test_1.21.1_1 <kristina@mincih.eu>
2025-05-03 15:37:50 +0200

    FUKC YOU GRADLE

--- src/main/java/felix/test/Test.java
+++ src/main/java/felix/test/Test.java
@@ -7,6 +7,9 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,12 +19,13 @@ public class Test implements ModInitializer {
     public static RegistryHelper registryHelper = new RegistryHelper();
     public static final Block VIBRANIUM_ORE_BLOCK = new Block(AbstractBlock.Settings.create());
     public static final Item RAW_VIBRAIUM_ITEM = new Item(new Item.Settings());
+    public static final Item SHIELD = new ShieldItem(new Item.Settings().maxDamage(2500));
 
     @Override
     public void onInitialize() {
         registryHelper.registerItem(new Item(new Item.Settings().food(ModFoodComponents.BURGER_ITEM)), "burger", "Burger");
         registryHelper.registerBlockAndItem(VIBRANIUM_ORE_BLOCK, "vibranium_ore_block", "Vibranium Ore");
         registryHelper.registerItem(RAW_VIBRAIUM_ITEM, "raw_vibranium", "raw vibranium");
-        registryHelper.registerItem(new Shield(new Item.Settings()), "shield", "Shield");
+        registryHelper.registerItem(SHIELD, "shield", "Shield");
     }
 }

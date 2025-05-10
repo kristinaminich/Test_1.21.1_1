7f2680ffa3ba7a6bc3abb87e768eec493da9f4eb
Test_1.21.1_1 <kristina@minich.eu>
2025-05-10 16:40:13 +0200

    FUCK

---
+++ src/main/java/felix/test/Item/ModToolMaterials.java
@@ -0,0 +1,64 @@
+package felix.test.Item;
+
+import com.google.common.base.Suppliers;
+import felix.test.Test;
+import net.minecraft.block.Block;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Supplier;
+
+import static net.minecraft.registry.tag.BlockTags.INCORRECT_FOR_NETHERITE_TOOL;
+
+public enum ModToolMaterials implements ToolMaterial {
+    QUADRATITE(INCORRECT_FOR_NETHERITE_TOOL,
+            16, 20.0F, 4.0F, 17, () -> Ingredient.ofItems(Test.QUADRATITE_INGOT));
+
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    ModToolMaterials(final TagKey<Block> inverseTag, final int itemDurability,final float miningSpeed,
+                     final float attackDamage, final int enchantability,final Supplier<Ingredient> repairIngredient) {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = Suppliers.memoize(repairIngredient::get);
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return this.inverseTag;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
--- src/main/java/felix/test/Test.java
+++ src/main/java/felix/test/Test.java
@@ -5,12 +5,16 @@ import felix.test.registry.RegistryHelper;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ShieldItem;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static felix.test.Item.ModToolMaterials.QUADRATITE;
 
 public class Test implements ModInitializer {
     public static final String MODID = "test";
@@ -19,19 +23,14 @@ public class Test implements ModInitializer {
     public static final Block VIBRANIUM_ORE_BLOCK = new Block(AbstractBlock.Settings.create());
     public static final Item RAW_VIBRAIUM_ITEM = new Item(new Item.Settings());
     public static final Item SHIELD = new ShieldItem(new Item.Settings().maxDamage(2500));
-    public static final ToolMaterial QUADRATITE_TOOL_MATERIAL = new ToolMaterial(
-            16,
-            20.0F,
-            0.0F,
-            15,
-            QuadritieArmorMaterial.REPAIRS_GUIDITE_ARMOR
-    );
-    public static final Item QUADRITITE_SWORD = new SwordItem((QUADRATITE_TOOL_MATERIAL, 6f, 1.6f), new Item.Settings());
+    public static final Item QUADRATITE_INGOT = new Item(new Item.Settings());
+    public static final Item QUADRITITE_SWORD = new SwordItem(QUADRATITE, new Item.Settings());
     @Override
     public void onInitialize() {
         registryHelper.registerItem(new Item(new Item.Settings().food(ModFoodComponents.BURGER_ITEM)), "burger", "Burger");
         registryHelper.registerBlockAndItem(VIBRANIUM_ORE_BLOCK, "vibranium_ore_block", "Vibranium Ore");
         registryHelper.registerItem(RAW_VIBRAIUM_ITEM, "raw_vibranium", "raw vibranium");
         registryHelper.registerItem(SHIELD, "shield", "Shield");
+        registryHelper.registerItem(QUADRITITE_SWORD, "quadratite_sword", "Quadratite Sword");
     }
 }
